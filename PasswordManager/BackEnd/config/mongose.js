8cfc4116614bf1cb081b8c35b104e19a668d9e85
Ankit Kumar <ankmail201@gmail.com>
2022-12-27 22:56:53 +0530

    bac

--- PasswordManager/BackEnd/config/mongose.js
+++ PasswordManager/BackEnd/config/mongose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/myapp');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
